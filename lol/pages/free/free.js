ff7f5f8ff9a4ac23ff7f3755b0fe143b5327dd4f
yilinhuang <531059877@qq.com>
2017-07-06 10:56:28 +0800

    Add files via upload

---
+++ lol/pages/free/free.js
@@ -0,0 +1,99 @@
+// pages/free/free.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    heros: [
+      {
+      img: "../images/2.png",
+      nickname: "1.Braum 布隆 弗雷尔卓德 Support"
+      },
+      {
+      img: "../images/3.png",
+      nickname: '2.Chogath 科加斯 虚空恐惧 Tank'
+      },{
+      img: "../images/4.png",
+      nickname: '3.Fiora 菲奥娜 无双剑姬 Fighter'
+      },{
+      img: "../images/5.png",
+      nickname: '4.Fizz 菲兹 潮汐海灵 Assassin'
+      },{
+      img: "../images/6.png",
+     nickname: '5.Graves 格雷福斯 法外狂徒 Markman'
+      },{
+      img: "../images/7.png",
+      nickname: '6.Jhin 烬 戏命师 Markman'
+      },{
+      img: "../images/8.png",
+      nickname: '7.Lulu 璐璐 仙灵女巫 Support'
+      },{
+      img: "../images/9.png",
+      nickname: '8.Morgana 莫甘娜 堕落天使 Mage'
+      },{
+      img: "../images/10.png",
+      nickname: '9.Nasus 内瑟斯 沙漠死神 Fighter'
+      },{
+      img: "../images/11.png",
+      nickname: '10.Zed 劫 影流之主 Assassin'
+       }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
---
+++ lol/pages/legends/legends.js
@@ -0,0 +1,95 @@
+// pages/legends/legends.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+      result:[
+      {
+        img:"../images/6.png",
+        date:"2017-06-10",
+        time:"12:45:50",
+        num:"1179991680",
+        result:"2/11/2 负"
+      },
+      {
+        img:"../images/8.png",
+        date:"2017-06-10",
+        time:"12-13-47",
+        num:"1179966154",
+        result:"6/10/3 负"
+      },
+      {
+        img:"../images/9.png",
+        date:"2017-06-10",
+        time:"11:44:10",
+        num:"1179973823",
+        result:" 2/5/7 胜"
+      },
+      {
+        img:"../images/10.png",
+        date:"2017-06-10",
+        time:"11:00:50",
+        num:"1179913902",
+        result:" 3/8/4 负"
+      }
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
